0af95d5b65d8477f3fd6b7a59e80c53cd9c6ab2f
PH0308 <162641504+PH0308@users.noreply.github.com>
2024-03-21 21:22:45 -0300

    Add files via upload

---
+++ AULA3/exemplo.js
@@ -0,0 +1,10 @@
+
+/*const nome = "pedro henrique nunes carlos"
+const idade = 15
+console.log("Olá1!", "Meu nome é ", nome, "e eu tenho", idade, "anos")*/
+
+const nome = "Pedro" 
+const sobrenome = "Nunes"
+const idade = "15"
+const souUmBomBoolean = true 
+console.log("Meu nome é", nome, sobrenome, "tenho", idade ,"anos de idade","sou estudante", souUmBomBoolean)
