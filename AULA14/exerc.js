5abf8a2ca4b9c4dc60fc9eef95db1a0e6fbef073
PedroNunes <162641504+PedroNunesc@users.noreply.github.com>
2025-02-15 18:58:48 -0300

    Delete AULA14 directory

--- AULA14/exerc.js
+++
@@ -1,7 +0,0 @@
-const readlineSync = require('readline-sync');
-const readlineSyn = require('readline-sync');
-
-const frase = readlineSync.question("Qual seu nome?");
-const frase6 = readlineSyn.question("Qual seu Email?")
-
-console.log(`O seu email ${frase6} fui cadastrado com sucesso ${frase}?`)
