ea6ed9205ce4d06b7051656cc3d95073830e8dd7
PedroNunes <162641504+PedroNunesc@users.noreply.github.com>
2025-02-15 18:58:07 -0300

    Delete AULA10 directory

--- AULA10/readline3.js
+++
@@ -1,8 +0,0 @@
-const rl = readline.createainterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question( "Digite o primeiro número:") (numero1), {
-    rl.question("Digite o segundo número:")
-}
