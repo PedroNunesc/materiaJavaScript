053e0e33a8b108ef07ed05596331daf992940f12
PedroNunes <162641504+PedroNunesc@users.noreply.github.com>
2025-02-15 18:59:48 -0300

    Delete AULA5 directory

--- AULA5/desafio1.js
+++
@@ -1,6 +0,0 @@
-let a = 3
-let b = 5
-const soma = a + b
-console.log(soma)
-//quando fazer soma fazer como esta acima
-//+ = +  - = -  x = *  divisão = /
--- AULA5/escrita3.js
+++
@@ -1,8 +0,0 @@
-let a = 10
-let b = 25
-c = b
-b = a 
-a = c
-console.log(a, b, c)
-//para essa troca de valores entre 'a' e 'b' precisou de mais um componente o 'c' para essa troca poder
-//ser feita
