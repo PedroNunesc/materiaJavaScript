cd97a99330e0ddf13c62b44449024de6235c9502
PedroNunes <162641504+PedroNunesc@users.noreply.github.com>
2025-02-15 18:59:40 -0300

    Delete AULA3 directory

--- AULA3/exemplo2.js
+++
@@ -1,9 +0,0 @@
-/*const numero1 = 5
-const numero2 = 10
-
-console.log (numero1 + numero2)*/
-
-const serie = "cobra kai"
-const temoporadas = 5
-console.log(typeof serie) //string
-console.log(typeof temoporadas) //number
