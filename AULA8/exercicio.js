76e6358bdbc0663bf7b0382f88983f9455ba9cda
PedroNunes <162641504+PedroNunesc@users.noreply.github.com>
2025-02-15 19:00:13 -0300

    Delete AULA8 directory

--- AULA8/exercicio.js
+++
@@ -1,4 +0,0 @@
-let comida = "queijo"
-if (comida === "queijo"){
-    console.log("Hoje você vai jantar bem!")
-}
--- AULA8/exercicio2.js
+++
@@ -1,6 +0,0 @@
-let time = "Inter"
-if (time === "Grêmio"){
-    console.log("Você torce pro maior do sul!")
-} else {
-    console.log("Você torce para o menor do sul!")
-}
